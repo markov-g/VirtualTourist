885eb81fe86564da4c1f5387f29e0a7d5ca5f528
Georgi Markov <markov.georgi@gmail.com>
2021-12-29 07:15:07 -0500

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Georgi Markov on 12/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
